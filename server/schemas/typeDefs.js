06444ff1052917b7f478a9231fa166ee3e933ea6
Matthew Damron <mattdamron@msn.com>
2021-04-06 16:08:26 -0600

    add authorization

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,10 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql `
+    type Auth {
+        token: ID!
+        user: User
+    }
     type User {
         _id: ID
         username: String
@@ -26,10 +30,18 @@ const typeDefs = gql `
         username: String
     }
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
     }
 `;
 
