54d2935bed5d164711b20e7b7956331585ff8f99
Matthew Damron <mattdamron@msn.com>
2021-04-09 15:16:24 -0600

    merged to main

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
@@ -21,7 +21,7 @@ module.exports = {
     }
 
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, process.env.JWT_SECRET, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
@@ -32,6 +32,6 @@ module.exports = {
   signToken: function({ username, email, _id }) {
     const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: expiration });
   }
 };
